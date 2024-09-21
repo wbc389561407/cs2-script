7d6f807f8bb83604e2a05c76ca467582a144b0f1
王冰臣 <389561407@qq.com>
2024-09-21 17:37:47 +0800

    修改按键逻辑 模拟真实按键

--- src/main/java/vip/xiaonuo/GlobalKeyListenerExample.java
+++ src/main/java/vip/xiaonuo/GlobalKeyListenerExample.java
@@ -7,28 +7,55 @@ package vip.xiaonuo;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+
 
 public class GlobalKeyListenerExample implements NativeKeyListener {
 
 
+    //创建一个 map 通过 NativeKeyEvent code 转换为 java reobt KeyEvent code
+    public static HashMap<Integer, Integer> keyPressedMap = new HashMap<Integer, Integer>();
+    public static HashMap<Integer, Integer> keyReleasedMap = new HashMap<Integer, Integer>();
+
+    static {
+        //按下指定件触发的动作
+        keyPressedMap.put(NativeKeyEvent.VC_Z, KeyEvent.VK_Z);
+        keyPressedMap.put(NativeKeyEvent.VC_J, KeyEvent.VK_J);
+
+        //弹起指定件触发的动作
+        keyReleasedMap.put(NativeKeyEvent.VC_A, KeyEvent.VK_A);
+        keyReleasedMap.put(NativeKeyEvent.VC_D, KeyEvent.VK_D);
+        keyReleasedMap.put(NativeKeyEvent.VC_W, KeyEvent.VK_W);
+        keyReleasedMap.put(NativeKeyEvent.VC_S, KeyEvent.VK_S);
+    }
+
     @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
         System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-        String keyText = NativeKeyEvent.getKeyText(e.getKeyCode());
-
-        //如果按下z键， 实现 一件跳投的功能
-        if(keyText.equals("Z")) {  //按下z键 一键跳投 可以修改
-            MyRobot.yijiantiaotou();
+//        String keyText = NativeKeyEvent.getKeyText(e.getKeyCode());
+        Integer code = keyPressedMap.get(e.getKeyCode());
+        if(code!= null && !MyRobot.virtuallyPressKey(code)){
+            //如果有映射关系,执行对应的方法
+            if(KeyEvent.VK_Z == code){
+                MyRobot.yijiantiaotou();
+            }
+            if(KeyEvent.VK_J == code){
+                MyRobot.yijianwtiaotou();
+            }
         }
-
-        if(keyText.equals("J")) {  //按下j键 一键W跳投 可以修改
-            MyRobot.yijianwtiaotou();
-        }
-
     }
+
 
     public void nativeKeyReleased(NativeKeyEvent e) {
         System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        String keyText = NativeKeyEvent.getKeyText(e.getKeyCode());
+        Integer code = keyReleasedMap.get(e.getKeyCode());
+        //急停 整体不好用
+        if(code!= null && !MyRobot.virtuallyReleaseKey(code)){
+            MyRobot.jiting(code);
+            System.out.println("手动释放键"+keyText);
+        }
     }
 
     public void nativeKeyTyped(NativeKeyEvent e) {
--- src/main/java/vip/xiaonuo/MyRobot.java
+++ src/main/java/vip/xiaonuo/MyRobot.java
@@ -1,8 +1,12 @@
 package vip.xiaonuo;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @Author: wangbingchen
@@ -10,45 +14,42 @@ import java.awt.event.KeyEvent;
  */
 public class MyRobot {
 
+    //定义一个静态的Robot对象
     private static Robot robot;
+
+    //是否是人类操作的状态属性
+//    public static boolean humanFlag = true;
+
+    //用map存放按下的键
+    public static Set<Integer> keyPressSet = new HashSet<>();
+    //用map存放弹起的键
+    public static Set<Integer> keyReleaseSet = new HashSet<>();
 
     static {
         try {
+            //创建Robot对象
             robot = new Robot();
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
     }
+
     //一键跳投
     public static void yijiantiaotou() {
         System.out.println("一键跳投");
-        //按下空格键
-        robot.keyPress(KeyEvent.VK_SPACE);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        robot.keyRelease(KeyEvent.VK_SPACE);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        //按下空格键 释放空格键
+        MyRobot.key(KeyEvent.VK_SPACE);
+        //延迟
+        MyRobot.sleep(30);
         //释放鼠标左键
         robot.mouseRelease(InputEvent.BUTTON1_MASK);
         //释放鼠标右键
         robot.mouseRelease(InputEvent.BUTTON3_MASK);
     }
 
+
     //w跳投
     public static void yijianwtiaotou() {
-//        Robot robot = null;
-//        try {
-//            robot = new Robot();
-//        } catch (AWTException e) {
-//            throw new RuntimeException(e);
-//        }
         System.out.println("w跳投");
         //按下W键
         robot.keyPress(KeyEvent.VK_W);
@@ -57,12 +58,68 @@ public class MyRobot {
         //释放鼠标左键
         robot.mouseRelease(InputEvent.BUTTON1_MASK);
         //释放W键
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        MyRobot.sleep(50);
         robot.keyRelease(KeyEvent.VK_W);
         robot.keyRelease(KeyEvent.VK_SPACE);
     }
+    public static void jiting(Integer code) {
+        MyRobot.sleep(50);
+        if(KeyEvent.VK_W == code) {
+            MyRobot.key(KeyEvent.VK_S);
+        }
+        if(KeyEvent.VK_S == code) {
+            MyRobot.key(KeyEvent.VK_W);
+        }
+        if(KeyEvent.VK_A == code) {
+            MyRobot.key(KeyEvent.VK_D);
+        }
+        if(KeyEvent.VK_D == code) {
+            MyRobot.key(KeyEvent.VK_A);
+        }
+    }
+
+    public static void jiting(String keyText) {
+
+        MyRobot.sleep(100);
+        if(keyText.equals("W")) {
+            MyRobot.key(KeyEvent.VK_S);
+        }
+        if(keyText.equals("S")) {
+            MyRobot.key(KeyEvent.VK_W);
+        }
+        if(keyText.equals("A")) {
+            MyRobot.key(KeyEvent.VK_D);
+        }
+        if(keyText.equals("D")) {
+            MyRobot.key(KeyEvent.VK_A);
+        }
+    }
+
+    private static void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void key(Integer keycode) {
+        System.out.println("放入缓存："+keycode);
+        keyPressSet.add(keycode);
+        keyReleaseSet.add(keycode);
+        System.out.println("放入缓存完成："+keycode);
+        robot.keyPress(keycode);
+        MyRobot.sleep(50); //这里暂且用 50 毫秒，可以根据实际情况调整 调整随机数 50 到 200
+        robot.keyRelease(keycode);
+    }
+
+    public static boolean virtuallyReleaseKey(Integer keyCode) {
+        return keyReleaseSet.remove(keyCode);
+    }
+
+    public static boolean virtuallyPressKey(Integer keyCode) {
+        return keyPressSet.remove(keyCode);
+    }
+
+
 }
